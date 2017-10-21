580d61c24fb48912e9f958b58af192edf5e29d12
Robert Castiblanco <ccrs2006@gmail.com>
2017-10-20 23:54:57 -0600

    uploaded all needed files

--- bamazon-manager.js
+++ bamazon-manager.js
@@ -7,7 +7,7 @@ let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "(m)Denver303#?*",
+    password: "",
     database: "bamazonDB"
 });
 
